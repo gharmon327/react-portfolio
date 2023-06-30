fca8b39f16a95bdd4136fe1291cabda045c1aca0
Greg Harmon <gregharmon@Gregs-MacBook-Pro.local>
2023-06-30 16:55:28 -0600

    fixed style bugs with social links

--- portfolio/src/pages/Resume/Resume.js
+++ portfolio/src/pages/Resume/Resume.js
@@ -1,10 +1,11 @@
 import resume from '../../images/resume.png'
+import "./resume.css"
 
 export default function Resume(){
     return(
     <div>
-        <h1>Resume</h1>
+        {/* <h1>Resume</h1> */}
         <p><a href="documents/Gregory J Harmon Resume 2023.pages" download>Click Here to Download my Resume</a></p>
-        <img src={resume} alt='resume'/>
+        <img src={resume} alt='resume' className='resumeImg'/>
     </div>)
 }
