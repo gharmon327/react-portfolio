83d4ccf0aa44dae79cc7ae0665f7c8976dd9ab72
Greg Harmon <gregharmon@Gregs-MacBook-Pro.local>
2023-06-30 16:43:52 -0600

    added styling to project page

--- portfolio/src/pages/Home/Home.js
+++ portfolio/src/pages/Home/Home.js
@@ -1,28 +1,57 @@
-import './home.css'
+import "./home.css";
+import skills from "../../images/skills.svg";
+import profile from "../../images/HARMGRE.jpg";
+import github from "../../images/github-mark.png"
+import linkedin from "../../images/linkedin.png"
 
-export default function Home(){
-    return (
-        <>
-        <h1> Gregory Harmons Portfolio </h1>
-        <h2>A Peek Into My World As A Software Engineer</h2>
-        <h2>Who Am I?</h2>
-        <p>I am a Software Engineer with a strong interest in Full Stack Development. My professional goal is to work in the industry of software development, where I can help solve problems to help our society move forward with tech advancements. I am a hard-working, organized, sociable, and determined person who enjoys working both individually and in a team environment. With my 6 years of experience in the logistics industry I know first hand what it takes to be successful in a fast paced environment solving complex issues and working cross functionally. I will apply these skills when programming a new web application that aims to provide a better user experience!</p>
-        <h2>Passions</h2>
-        <ul>
-            <li>Hiking</li>
-            <li>Biking</li>
-            <li>Paddle-boarding</li>
-            <li>Camping</li>
-            <li>Coding</li>
-            <li>Fishing</li>
-            <li>Sex</li>
-        </ul>
-        <h2>Contact Me</h2>
-        <ul>
-            <li><b>Email:</b> GHarmon327@gmail.com</li>
-            <li><b>Location:</b> South Carolina(Open to Relocation)</li>
-        </ul>
-       
-        </>
-    )
+export default function Home() {
+  return (
+    <>
+      <h1> Gregory Harmons Portfolio </h1>
+      <div className="header">
+        <img className="headshot" src={profile} alt="headshot" />
+        <div className="who">
+          <h2>Who Am I?</h2>
+          <p className="brand">
+            I am a Software Engineer with a strong interest in Full Stack
+            Development. My professional goal is to work in the industry of
+            software development, where I can help solve problems to help our
+            society move forward with tech advancements. I am a hard-working,
+            organized, sociable, and determined person who enjoys working both
+            individually and in a team environment. With my 6 years of
+            experience in the logistics industry I know first hand what it takes
+            to be successful in a fast paced environment solving complex issues
+            and working cross functionally. I will apply these skills when
+            programming a new web application that aims to provide a better user
+            experience!
+          </p>
+        </div>
+      </div>
+      <h2>Skills</h2>
+      <img src={skills} alt="skills" />
+      <h2>Passions</h2>
+      <ul>
+        <li>🥾 Hiking 🥾</li>
+        <li>🚴 Biking 🚴</li>
+        <li>🏄 Paddle-boarding 🏄</li>
+        <li>🏕️ Camping 🏕️</li>
+        <li>👨‍💻 Coding 👨‍💻</li>
+        <li>🎣 Fishing 🎣</li>
+      </ul>
+
+    <div>
+      <h2>Contact Me</h2>
+      <ul>
+        <li>
+          📧 <b>Email:</b> GHarmon327@gmail.com 📧
+        </li>
+        <li>
+          🌎 <b>Location:</b> South Carolina(Open to Relocation) 🌎
+        </li>
+      </ul>
+        <a href="https://github.com/gharmon327"><img src={github} alt="github" height="50px" width="50px"/></a>
+        <a href="https://www.linkedin.com/in/gregjharmon/"><img src={linkedin} alt="linkedIN" height="50px" width="50px"/></a>
+    </div>
+    </>
+  );
 }
--- portfolio/src/pages/Projects/Projects.js
+++ portfolio/src/pages/Projects/Projects.js
@@ -1,30 +1,75 @@
-import fetch from '../../images/fetch.png'
-import clowns from '../../images/clowning.png'
-import recipe from '../../images/recipe.png'
-import blackjack from '../../images/blackjack.png'
+import fetch from "../../images/fetch.png";
+import clowns from "../../images/clowning.png";
+import recipe from "../../images/recipe.png";
+import blackjack from "../../images/blackjack.png";
+import "./projects.css";
 
-export default function Projects(){
-    return (
+export default function Projects() {
+  return (
     <div>
-        <h1>Projects</h1>
-        <p>While completing my Boot-camp with General Assembly I was able to work on Four projects, Two were individual projects and Two were Group projects. Check them out Below!</p>
-        <a href="https://clowning-around-client.onrender.com/">Clowning Around</a><br/>
-        <p>Clowning Around is the clown-themed forum that you never knew you needed. This was my third project and is a group project that was built with the git forking workflow. It is a full-stack app that was created using the MERN stack. What started as a joke ended up being one of the most enjoyable projects I worked on. Our team added a sense of humor to our work which made it fun. We were a team of 4 and got a lot of practice working in a group environment and troubleshooting each other's code.</p>
-        <img src={clowns} alt='clowns' height='500px' width='500px'/><hr/>
+      {/* <h1>Projects</h1> */}
+      {/* <p>
+        While completing my Boot-camp with General Assembly I was able to work
+        on Four projects, Two were individual projects and Two were Group
+        projects. Check them out Below!
+      </p> */}
 
-        <a href="https://recipe-forum-client.onrender.com/">Recipe Forum</a><br/>
-        <p>My team and I created a recipe forum application for my final project. Food has always been a place for people to come together, so we decided to make an app that allows people to share recipes and add comments and feedback on each recipe. This app was built with react using HTML, CSS, JS, Python, and Django and is hosted on a postgresSQL database. In addition, we implemented user image input utilizing Cloudinary, a massive win for our team!</p>
-        <img src={recipe} alt='recipe' height='500px' width='500px'/><hr/>
+      <a href="https://clowning-around-client.onrender.com/" className="link">Clowning Around</a>
+    <div className="project">
+      <br />
+      
+      <p>
+        Clowning Around is the clown-themed forum that you never knew you
+        needed. This was my third project and is a group project that was built
+        with the git forking workflow. It is a full-stack app that was created
+        using the MERN stack. What started as a joke ended up being one of the
+        most enjoyable projects I worked on. Our team added a sense of humor to
+        our work which made it fun. We were a team of 4 and got a lot of
+        practice working in a group environment and troubleshooting each other's
+        code.
+      </p>
+      <img src={clowns} alt="clowns" height="500px" width="500px" />
+      
+      </div>
 
-        <a href="https://gharmon327.github.io/blackjack-project/">BlackJack</a><br/>
-        <p>This was the first project I built and was one of my two solo projects. For this project, I was able to code one of my favorite card games from growing up using HTML, vanilla JS, and CSS. This project was so much fun; with this being my first go at making a game, there was plenty of opportunity to solve problems and grow as a software engineer. In addition, doing this allowed me to dive in and solidify my passion for engineering.</p>
-        <img src={blackjack} alt='blackjack' height='500px' width='500px'/><hr/>
+      <a href="httpas://recipe-forum-client.onrender.com/" className="link">Recipe Forum</a>
+      <div className="project">
+      <br />
+      <p>
+        My team and I created a recipe forum application for my final project.
+        Food has always been a place for people to come together, so we decided
+        to make an app that allows people to share recipes and add comments and
+        feedback on each recipe. This app was built with react using HTML, CSS,
+        JS, Python, and Django and is hosted on a postgresSQL database. In
+        addition, we implemented user image input utilizing Cloudinary, a
+        massive win for our team!
+      </p>
+      <img src={recipe} alt="recipe" height="500px" width="500px" />
+      </div>
 
-        <h2>Hackathon</h2>
-        <a href="https://pet-front-end.onrender.com/">Fetch</a><br/>
-        <p></p>
-        <img src={fetch} alt='fetch' height='500px' width='500px'/>
-        
+      <a href="https://gharmon327.github.io/blackjack-project/" className="link">BlackJack</a>
+      <div className="project">
+      <br />
+      <p>
+        This was the first project I built and was one of my two solo projects.
+        For this project, I was able to code one of my favorite card games from
+        growing up using HTML, vanilla JS, and CSS. This project was so much
+        fun; with this being my first go at making a game, there was plenty of
+        opportunity to solve problems and grow as a software engineer. In
+        addition, doing this allowed me to dive in and solidify my passion for
+        engineering.
+      </p>
+      <img src={blackjack} alt="blackjack" height="500px" width="500px" />
+     </div>
+
+      {/* <h2>Hackathon</h2> */}
+       <a href="https://pet-front-end.onrender.com/" className="link">Fetch</a>
+      <div className="project">
+     
+      <br />
+      <p>text for fetch</p>
+      <img src={fetch} alt="fetch" height="500px" width="500px" />
+      </div>
     </div>
-    )
+  );
 }
